946de6a42c8596131ca5c2afda0d29be66c4f755
codewithemma <codewithemma@gmail.com>
2022-11-10 09:58:21 +0100

    second commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import SignUp from "./Components/SignUp";
+function App() {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
